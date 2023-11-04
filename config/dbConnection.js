a65cd46c2002fbd0e16ff56e41f65baa339aa715
Vivek <vivekraj78965.vr@gmail.com>
2023-11-04 13:12:43 +0530

    db connection wit already created database

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,17 +2,15 @@ const mysql = require("mysql");
 
 const con = mysql.createConnection({
   host: process.env.LOCALHOST,
+  port: process.env.DB_PORT,
+  database:process.env.DATABASE,
   user: process.env.USER,
   password: process.env.PASSWORD
 });
 
 con.connect((err) => {
   if (err) throw err;
-  console.log("Connected");
-  con.query("CREATE DATABASE creditApproval", (err, result) => {
-    if (err) throw err;
-    console.log("Database Created!");
-  });
+  console.log("Connected with DB creditApproval");
 });
 
 module.exports = con;
